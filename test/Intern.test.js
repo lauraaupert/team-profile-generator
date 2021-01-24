63dc09a42f191fc1a5d36da4bf8f0792bebcfe0d
Laura Aupert <aupert.laura@gmail.com>
2021-01-24 18:56:46 -0500

    Pass all intern tests

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,18 +1,23 @@
 const Intern = require('../lib/intern')
 
 describe("Intern", () => {
-   describe("getName", () => {
-    it("", () => {
-        
-        expect().toEqual()
-    })
+   describe("getSchool", () => {
+        it("should return school name", () => {
+            const test = "Mesopotamia"
+            const intern = new Intern("Richard", "23", "moon@mask.com", "Intern", test)
+            
+            expect(intern.role).toEqual(test)
+        })
+
 
     })
     describe("getRole", () => {
-    it("", () => {
-        
-        expect().toEqual()
-    })
+        it("should return role as intern", () => {
+            const intern = new Intern("Richard", "23", "moon@mask.com", "MisterPenguin", "Intern")
+            
+            expect(intern.role).toEqual("Intern")
+        })
+
 
 })
 
