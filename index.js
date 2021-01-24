6e892a71a08fb55c2535e43a7803aea0ef9a3607
Laura Aupert <aupert.laura@gmail.com>
2021-01-24 16:56:21 -0500

    With all prompt questions correctly triggered and triggering proper functions. Created team array with all new Employees() being pushed.

--- index.js
+++ index.js
@@ -10,58 +10,142 @@ var team = []
 //need to push to team array from employee class? from somewhere else
 
 function initialize() {
+    addManager()
+}
+
+function addManager() {
+    inquirer
+    .prompt([
+        {        
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?"
+        },
+        {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID number?"
+        },
+        {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email address?"
+        },
+        {
+        type: "input",
+        name: "office",
+        message: "What is the manager's office number?"
+        },
+        {
+        type: "rawlist",
+        name: "addMember",
+        message: "Do you wish to add a new team member?",
+        choices: [
+            "Yes",
+            "No",
+            ]
+        }
+    ])
+        .then((response) => {
+            const manager = new Manager(
+                response.name,
+                response.id,
+                response.email,
+                response.office,
+                "Manager"
+                );                    
+            console.log(manager)
+            team.push(manager)
+            if (response.addMember === "Yes") {
+                addMember()
+            }
+        })
+        //.then(addMember())
+            
+}
+
+function oneMore() {
+    inquirer
+    .prompt([
+        {
+        type: "rawlist",
+        name: "addMember",
+        message: "Do you wish to add a new team member?",
+        choices: [
+            "Yes",
+            "No",
+            ]
+        }
+    ]).then((response) => {
+        if (response.addMember === "Yes") {
+            addMember()
+        } else {
+            console.log(team)
+        }
+
+    })
+}
+
+
+
+     
+
+function addMember() {
 inquirer
     .prompt([
         {
         type: "rawlist",
         name: "type",
-        message: "Do you wish to add a new team member?",
+        message: "What is the role of the new team member?",
         choices: [
-            "Manager",
             "Engineer",
             "Intern",
-            "No more team members, enough humans"
             ]
         },
         {
         type: "input",
         name: "name",
-        message: "What is your name?"
+        message: "What is the employee's name?"
         },           
         {
         type: "input",
         name: "id",
-        message: "What is your ID number?"
+        message: "What is this employee's ID number?"
         },
         {
         type: "input",
         name: "email",
-        message: "What is your email address?"
-        }
+        message: "What is the employee's email address?"
+        },
     ])
     .then((response) => {
         switch (response.type) {
-            case "Manager":
+            case "Engineer":
                 inquirer
                 .prompt([
                   {
                     type: "input",
-                    name: "office",
-                    message: "What is your office number?"
+                    name: "github",
+                    message: "What is the Engineer's Github profile name?"
                   }
                 ])
                 .then((val) => {
                   //const office = val.office;
                   //console.log(officeNum);
-                  const manager = new Manager(
+                  const engineer = new Engineer(
                     response.name,
                     response.id,
                     response.email,
-                    val.office,
+                    val.github,
                     response.type
                   );                    
-                 console.log(manager)
-                  })
+                 console.log(engineer)
+                 team.push(engineer)
+                 oneMore()
+                 })
+                
+    
+            
                 break;
     
             case "Intern":
@@ -84,10 +168,15 @@ inquirer
                         response.type
                       );                    
                      console.log(intern)
+                     team.push(intern)
+                        oneMore()
                     })
+                    
                     break;
                 }
+
             })
+            
         }
                       
     
